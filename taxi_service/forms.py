e3ade0b6850c648d00ae7b5d8012eae8d24be1ca
KurochkaR <kurochkar001@gmail.com>
2023-03-05 15:02:45 +0200

    resolve conflicts add translation for fields and errors

--- taxi_service/forms.py
+++ taxi_service/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 from app.models import Order, SubscribeUsers, User
+from django.utils.translation import gettext_lazy as _
 
 
 class MainOrderForm(ModelForm):
@@ -10,31 +11,31 @@ class MainOrderForm(ModelForm):
         fields = ('from_address', 'phone_number')
         error_messages = {
             "from_address": {
-                "required": "Enter your address please",
+                "required": _("Введіть будь-ласка адресу"),
             },
             "phone_number": {
-                "required": "Enter your phone please",
+                "required": _("Введіть будь-ласка ваш номер телефону"),
             },
         }
 
         widgets = {
             'from_address': forms.TextInput(attrs={
-                'id': 'address', 'class': 'form-control', 'placeholder': 'Pickup Location', 'style': 'font-size: medium'}),
+                'id': 'address', 'class': 'form-control', 'placeholder': _('Ваша адреса'), 'style': 'font-size: medium'}),
             'phone_number': forms.NumberInput(attrs={
-                'id': 'phone', 'class': 'form-control', 'placeholder': 'Phone Number', 'style': 'font-size: medium'})
+                'id': 'phone', 'class': 'form-control', 'placeholder': _('Номер телефону'), 'style': 'font-size: medium'})
         }
 
     def clean_phone_number(self):
         phone_number = self.cleaned_data.get('phone_number')
         if User.phone_number_validator(phone_number) is None:
-            raise forms.ValidationError("Wrong phone format")
+            raise forms.ValidationError(_("Номер телефону невірний"))
         else:
             return User.phone_number_validator(phone_number)
 
     def clean_from_address(self):
         from_address = self.cleaned_data.get('from_address')
         if not len(from_address):
-            raise forms.ValidationError("Wrong address")
+            raise forms.ValidationError(_("Неправильна адреса"))
         else:
             return from_address
 
@@ -42,12 +43,12 @@ class MainOrderForm(ModelForm):
 class SubscriberForm(ModelForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={
             'class': 'form-control',
-            'placeholder': 'Enter email',
+            'placeholder': _('Введіть пошту'),
             'style': 'font-size: medium',
             'id': 'sub_email'
         }),
-        error_messages={'required': 'Enter your email please',
-                        'invalid': 'Enter correct email'}
+        error_messages={'required': _('Введіть ел.пошту будь-ласка'),
+                        'invalid': _('Введіть корректну ел.пошту')}
     )
 
     class Meta:
@@ -57,8 +58,8 @@ class SubscriberForm(ModelForm):
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if User.email_validator(email) is None:
-            raise forms.ValidationError("Wrong email address")
+            raise forms.ValidationError(_("Невірний формат ел.пошти"))
         elif SubscribeUsers.get_by_email(email) is not None:
-            raise forms.ValidationError("You are subscriber already")
+            raise forms.ValidationError(_("Ви вже підписались"))
         else:
             return email
